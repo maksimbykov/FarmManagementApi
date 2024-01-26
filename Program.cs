3cc2389988fd2287d6a90f3e2276685d4b4b5e60
Max Bykov <mbykov@transperfect.com>
2024-01-26 04:15:00 +0400

    working prototype

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AnimalDbContext>(opt => opt.UseInMemoryDatabase("FarmManagementDb"));
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
